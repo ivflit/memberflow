0aeaff28270a5d0c7c2f52d190835cdd6169f2b2
Ivan Flitcroft <Ivanflitcroft@gmail.com>
2026-04-01 15:27:03 +0100

    Removed transition css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,5 @@
 <template>
-  <RouterView v-slot="{ Component }">
-    <Transition
-      name="page-fade"
-      mode="out-in"
-    >
-      <component :is="Component" />
-    </Transition>
-  </RouterView>
+  <RouterView />
   <DevToggle />
 </template>
 
